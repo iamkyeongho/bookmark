3fd3b21d14e07093c03a00ec6deaba4fb2eff4ea
iamkyeongho <iamkyeongho@gmail.com>
2022-10-14 14:07:48 +0900

    add static_root

--- config/settings.py
+++ config/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "statif_files")
+STATIC_ROOT = os.path.join(BASE_DIR, "static_files")
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 # Default primary key field type
