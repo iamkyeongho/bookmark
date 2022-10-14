65102637e8d193e4e346f1764bc27a4a731d8830
iamkyeongho <iamkyeongho@gmail.com>
2022-10-14 13:49:35 +0900

    호스트 변경

--- config/settings.py
+++ config/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-1&byp1-s=8*e7cwo^nnbnxyv%5b90t7#h%0vkyxn((8nuj**#q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 #ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
